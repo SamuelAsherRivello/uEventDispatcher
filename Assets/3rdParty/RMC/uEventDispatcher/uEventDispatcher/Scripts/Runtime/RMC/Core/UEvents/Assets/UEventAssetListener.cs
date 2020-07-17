67ce66d76f42c21ab842d87b90f383ae95795423
srivello3 <sam@rivello.org>
2020-07-16 22:19:00 -0400

    Add EventAssetListener concept

---
+++ Assets/3rdParty/RMC/uEventDispatcher/uEventDispatcher/Scripts/Runtime/RMC/Core/UEvents/Assets/UEventAssetListener.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace RMC.Core.UEvents.Assets
+{
+	public class UEventAssetListener : MonoBehaviour
+	{
+		//  Fields ---------------------------------------
+		[SerializeField]
+		private UEventAsset _uEventAsset = null;
+
+		[SerializeField]
+		private UEvent _uEvent = new UEvent();
+
+		//  Unity Methods   -------------------------------
+		protected void OnEnable()
+		{
+			_uEventAsset.AddListener(OnEvent);
+		}
+
+		protected void OnDisable()
+		{
+			_uEventAsset.RemoveListener(OnEvent);
+		}
+
+		//  Event Handlers   -------------------------------
+		private void OnEvent(IUEventData uEventData)
+		{
+			_uEvent.Invoke(uEventData);
+		}
+	}
+}
